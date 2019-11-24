eea66e13c95d6854603c13a5632d70a1a782fbba
ruk <ruk.lakhani@students.makeschool.com>
2019-11-24 15:19:33 -0800

    read playlists

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template('home.html', msg='Playlister!')
+
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
+
+if __name__ == '__main__':
+    app.run(debug=True)
