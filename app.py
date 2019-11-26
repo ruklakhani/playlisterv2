5fc11026aba7d0578e42477fdc34d4d8883bb867
ruk <ruk.lakhani@students.makeschool.com>
2019-11-26 14:13:44 -0800

    able to use playlists

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for
+from bson import ObjectId
+import os
+
+app = Flask(__name__)
 
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
-
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
 
 def video_url_creator(id_lst):
     videos = []
@@ -16,38 +17,86 @@ def video_url_creator(id_lst):
         videos.append(video)
     return videos
 
-# OUR MOCK ARRAY OF PROJECTS
-# playlists = [
-#   { 'title': 'Great Playlist' },
-#   { 'title': 'Next Playlist' }
-# ]
-
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
-# Note the methods parameter that explicitly tells the route that this is a POST
+
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
     """Submit a new playlist."""
-    # Grab the video IDs and make a list out of them
-    video_ids = request.form.get('video_ids').split()
-    # call our helper function to create the list of links
-    videos = video_url_creator(video_ids)
     playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': videos,
-        'video_ids': video_ids
+        'videos': request.form.get('videos').split(),
+        'created_at': datetime.now()
     }
-    playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
 
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
+
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>')
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist}
+    )
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'playlist_id': ObjectId(request.form.get('playlist_id'))
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+
+
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
